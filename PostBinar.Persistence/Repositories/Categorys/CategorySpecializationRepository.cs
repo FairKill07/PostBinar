d0d0e2d637d5e58c0554b69978b7a1f814b4e588
Олег Насатов <nasatov.oleg03@gmail.com>
2025-10-23 00:14:44 +0300

    ref: category repository

--- PostBinar.Persistence/Repositories/Categorys/CategorySpecializationRepository.cs
+++ PostBinar.Persistence/Repositories/Categorys/CategorySpecializationRepository.cs
@@ -3,34 +3,48 @@ using PostBinar.Application.Abstractions.Interfaces.Repositories;
 using PostBinar.Domain.Categorys;
 using PostBinar.Persistence.DbContects;
 
-namespace PostBinar.Persistence.Repositories.Categorys;
+namespace PostBinar.Persistence.Repositories;
 
 internal sealed class CategorySpecializationRepository : ICategorySpecializationRepository
 {
     private readonly PostBinarDbContext _context;
+
     public CategorySpecializationRepository(PostBinarDbContext context)
     {
         _context = context;
     }
+
     public void Add(Specialization specialization)
     {
         _context.CategorySpecializations.Add(specialization);
     }
+
     public void Delete(Specialization specialization)
     {
         _context.CategorySpecializations.Remove(specialization);
     }
-    public async Task<Specialization> GetByIdAsync(int categorySpecializationId)
+
+    public async Task<Specialization> GetByIdAsync(
+        int categorySpecializationId,
+        CancellationToken cancellationToken = default)
     {
-        var specialization = await _context.CategorySpecializations.FindAsync(categorySpecializationId);
-        if (specialization == null)
-        {
+        var specialization = await _context.CategorySpecializations
+            .AsNoTracking()
+            .FirstOrDefaultAsync(s => s.Id == categorySpecializationId, cancellationToken);
+
+        if (specialization is null)
             throw new InvalidOperationException($"Specialization with ID {categorySpecializationId} not found.");
-        }
+
         return specialization;
     }
-    public async Task<List<Specialization>> GetAllAsync()
+
+    public async Task<IReadOnlyList<Specialization>> GetAllAsync(
+        CancellationToken cancellationToken = default)
     {
-        return await _context.CategorySpecializations.ToListAsync();
+        IQueryable<Specialization> query = _context.CategorySpecializations;
+
+        return await query
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
     }
 }
